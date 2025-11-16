a1711176afc8cb08576bfa3638d917929ef47ba0
Krish120003 <krish120003@gmail.com>
2025-11-16 13:18:02 -0500

    feat: update runtime env

--- src/env.js
+++ src/env.js
@@ -38,7 +38,7 @@ export const env = createEnv({
     AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
     AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
-    VERCEL_TOKEN: process.env.VERCEL_TOKEN,
+    VERCEL_TOKEN: process.env.VERCEL_TOKEN ?? process.env.VERCEL_OIDC_TOKEN,
     NODE_ENV: process.env.NODE_ENV,
   },
   /**
